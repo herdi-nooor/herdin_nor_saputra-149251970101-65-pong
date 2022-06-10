a20487223538bcfe19201ad6d4c2eee9813399ec
herdi-nooor <herdinoor369@gmail.com>
2022-06-10 17:41:56 +0800

    add UI & Game Manajer, and little edit on resolution screen

--- Assets/Script/ScoreController.cs
+++ Assets/Script/ScoreController.cs
@@ -6,13 +6,16 @@ using UnityEngine.UI;
 public class ScoreController : MonoBehaviour
 {
     public Text LeftScore; 
-    public Text RightScore; 
+    public Text RightScore;
+    public Text paddlespeed; 
  
     public ScoreManager manager;
+    public padlleController padlle;
 
     private void Update() 
     { 
         LeftScore.text = manager.leftScore.ToString(); 
         RightScore.text = manager.rightScore.ToString(); 
+        paddlespeed.text = ("padle speed : " + padlle.speed.ToString());
     } 
 }
